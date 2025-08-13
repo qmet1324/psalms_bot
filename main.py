49cf4adb7938565110490a3438d9623c60a748a7
qmet1324 <mikhail.melnik.gamedev@gmail.com>
2025-08-12 21:16:39 -0400

    feat: new command for requesting data from API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 import telebot
 
@@ -6,12 +7,14 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-@bot.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how you doing?")
+@bot.message_handler(commands=['verse'])
+def send_verse(message):
+    response = requests.get("https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-asv/books/genesis/chapters/1/verses/1.json")
+    data = response.json()
+    bot.reply_to(message, data["text"])
 
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+# @bot.message_handler(func=lambda msg: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
 
 bot.infinity_polling()
