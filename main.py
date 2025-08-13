7331af36265270bfcbf9071f6299ee3b153900ba
qmet1324 <mikhail.melnik.gamedev@gmail.com>
2025-08-12 21:30:54 -0400

    feat: menu with available commands and description

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+bot.set_my_commands(
+    commands = [
+        telebot.types.BotCommand('hello', 'Say Hi to PsalmsBot'),
+        telebot.types.BotCommand('verse', 'Receive a random psalm'),
+    ],
+)
+
+@bot.message_handler(commands=['hello'])
+def send_verse(message):
+    bot.reply_to(message, "Greetings, the Son of God") 
+
 @bot.message_handler(commands=['verse'])
 def send_verse(message):
     response = requests.get("https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-asv/books/genesis/chapters/1/verses/1.json")
     data = response.json()
     bot.reply_to(message, data["text"])
-
-# @bot.message_handler(func=lambda msg: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
 
 bot.infinity_polling()
